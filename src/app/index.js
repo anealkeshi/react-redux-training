f477247e9afe55fbe3c4e4efd638a1615bd0fc10
neal <anilksee@gmail.com>
2017-10-03 22:41:49 -0400

    Separating compoenents and folder structure

--- src/app/index.js
+++ src/app/index.js
@@ -1,72 +1,12 @@
-import React from "react";
 import {render} from "react-dom";
-import {createStore, combineReducers, applyMiddleware} from "redux";
-import {createLogger} from "redux-logger";
-
-import App from "./components/App";
+import React from "react";
 import {Provider} from "react-redux";
 
-const mathReducer = (state = {
-  result: 1,
-  lastValues: []
-}, action) => {
-    switch (action.type) {
-        case "ADD":
-            state = {
-              ...state,
-              result: state.result + action.payload,
-              lastValues: [...state.lastValues, action.payload]
-            };
-            break;
-        case "SUBTRACT":
-        state = {
-          ...state,
-          result: state.result - action.payload,
-          lastValues: [...state.lastValues, action.payload]
-        };
-            break;
-    }
-    return state;
-};
+import App from "./containers/App";
+import store from "./store";
 
-const userReducer = (state = {
-  name: "Max",
-  age: 27
-}, action) => {
-    switch (action.type) {
-        case "SET_NAME":
-            state = {
-              ...state,
-              name: action.payload
-            };
-            break;
-        case "SET_AGE":
-        state = {
-          ...state,
-          age: action.payload
-        };
-            break;
-    }
-    return state;
-};
-
-const myLogger = (store) => (next) => (action) => {
-  console.log("Logged Action: ", action);
-  next(action);
-}
-
-const logger = createLogger();
-
-const store = createStore(combineReducers(
-  {math: mathReducer, user: userReducer}),
-   {},
-   applyMiddleware(myLogger, logger));
-
-store.subscribe(() => {
-    //console.log("Store updated!", store.getState());
-});
-
-
-render(<Provider store={store} >
-          <App />
-        </Provider>, window.document.getElementById('app'));
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    window.document.getElementById('app'));
